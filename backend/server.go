577c45b39de04876caf358cd27fe28ae5ab43fe1
hokita <hideee.0202@gmail.com>
2024-12-30 07:51:20 +0900

    Create Backend.

---
+++ backend/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/hokita/owl/graph"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	dsn := "owl:Owl1234-@tcp(192.168.1.101:3306)/owl?charset=utf8mb4&parseTime=True&loc=Local"
+	// データベースに接続
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("データベースへの接続に失敗しました: %v", err)
+	}
+	defer db.Close()
+
+	// 接続を確認
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("データベースへの接続確認に失敗しました: %v", err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{DB: db}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
