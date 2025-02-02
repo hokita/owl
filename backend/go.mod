0670a11d7f69673d12573b9e373207fde546ebe1
hokita <hideee.0202@gmail.com>
2025-02-02 10:17:01 +0900

    Add environment settings to the backend.

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/99designs/gqlgen v0.17.55
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
 	github.com/rs/cors v1.11.1
 	github.com/vektah/gqlparser/v2 v2.5.17
 )
--- backend/server.go
+++ backend/server.go
@@ -2,32 +2,65 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/hokita/owl/graph"
+	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
 
 func main() {
-	dsn := "owl:Owl1234-@tcp(192.168.1.101:3306)/owl?charset=utf8mb4&parseTime=True&loc=Local"
-	// データベースに接続
+	// Get environment variables
+	env := os.Getenv("ENV")
+	if env == "" {
+		env = "development"
+	}
+
+	if env != "production" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatalf("failed to load .env: %v", err)
+		}
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		log.Fatalf("DB_USER is not set")
+	}
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		log.Fatalf("DB_PASSWORD is not set")
+	}
+	dbEndpoint := os.Getenv("DB_ENDPOINT")
+	if dbEndpoint == "" {
+		log.Fatalf("DB_ENDPOINT is not set")
+	}
+	corsAlllowOrigins := os.Getenv("CORS_ALLOW_ORIGINS")
+	if corsAlllowOrigins == "" {
+		log.Fatalf("CORS_ALLOW_ORIGINS is not set")
+	}
+
+	// Connect to the database
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/owl?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbEndpoint)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		log.Fatalf("データベースへの接続に失敗しました: %v", err)
+		log.Fatalf("failed to open a database connection: %v", err)
 	}
 	defer db.Close()
 
-	// 接続を確認
+	// Check the connection
 	err = db.Ping()
 	if err != nil {
-		log.Fatalf("データベースへの接続確認に失敗しました: %v", err)
+		log.Fatalf("failed to ping the database: %v", err)
 	}
 
 	port := os.Getenv("PORT")
@@ -39,13 +72,15 @@ func main() {
 
 	// Setup CORS
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173", "http://192.168.1.101:5173"},
+		AllowedOrigins:   strings.Split(corsAlllowOrigins, ","),
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 	})
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	if env == "development" {
+		http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	}
 	http.Handle("/query", c.Handler(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
